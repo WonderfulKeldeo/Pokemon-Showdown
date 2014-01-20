2ac6dad3a764c63ec8f40dca8e33a883ba9aa135
jd4564 <thelittlejd09@gmail.com>
2014-01-20 17:04:48 -0400

    Tidy up that weird spacing.
    Conflicts:
    	commands.js

--- commands.js
+++ commands.js
@@ -3410,4 +3410,8 @@ function escapeHTML(target) {
 	target = target.replace(/>/g, '&gt;');
 	target = target.replace(/"/g, '&quot;');
 	return target;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> eddb53e... Tidy up that weird spacing.
